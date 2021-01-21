dd4599d90ff9f2cfc7e5907985f99a1613b3d656
WendysNeverland <s_wen@msn.com>
2021-01-21 14:12:07 +0100

    module 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-export function fetchMovies(){
+import * as service from './services';
 
+let movies = require('./data/movies.json');
+
+
+export function fetchMovies(){
+    const resolveFunction = () => movies;
+
+    service.fetchWithTimeout(1000).then(resolveFunction);
 }
+
+let moviePromise = fetchMovies();
+moviePromise.then(function resolved(results){console.log(results)})
